fbb1876e20d667bbca48b53d0534b45c9b419f41
Joel Alftberg <joel.alftberg98@gmail.com>
2025-10-17 15:34:53 +0200

    Finishes inlamning p1

--- Inlämning-P1/yrgo/yrgo/main.c
+++ Inlämning-P1/yrgo/yrgo/main.c
@@ -1,58 +1,70 @@
 #include <avr/io.h>
 
 #include "p1/P1.h"
-#include <time.h>
 #include <util/delay.h>
-#include <stdbool.h>
-#include <stdio.h>
 
 int main(void)
 {
+
     uint8_t led_number = 1;
     uint16_t led_timer = 0;
+    uint16_t pot_timer = 0;
     uint8_t duty_cycle = 0;
-    
+    uint16_t delay = 0;
+    uint16_t intensity = 0;
+
     setup_p1();
     
-    while(1) {
-        
-        uint16_t delay = 50 * read_pot1();
-        uint16_t intensity = read_pot2() / 10;
-        
-        if (intensity > 100) intensity = 100; 
-        
+    while (1){
+
         led_timer++;
+        pot_timer++;
         
-        if (!sw3())
-        {                
+        if (pot_timer >= 1500){
+
+            delay = 50 * read_pot1();
+            intensity = read_pot2() / 10;
+
+            /* Clamp intensity so that duty cycle never goes over 100% */
+            if (intensity > 100){
+                intensity = 100;
+            }
+
+            pot_timer = 0;
+
+        }
+        
+        if (!sw3()){
+            
             /* PWM */
-            
             duty_cycle < intensity ? led(led_number, true) : led(led_number, false);
-            
             duty_cycle++;
             
-            if(duty_cycle >= 100) duty_cycle = 0;
+            if (duty_cycle >= 100){
+                duty_cycle = 0;
+            }
             
         }
 
-        if(!sw1() && led_timer >= delay){
+        /* LED cycling */
+        if (!sw1() && led_timer >= delay){
             
             led(0, false);
 
+            sw2() ? led_number-- : led_number++;
+
             if (led_number > 3){
                 led_number = 1;
-            }            
-            else if(led_number < 1) {
+            }
+            else if (led_number < 1){
                 led_number = 3;
-            }            
-            
-            sw2() ? led_number-- : led_number++;
+            }
+
             led_timer = 0;
-            duty_cycle = 0;
             
         }
         
-        _delay_us(100);
+        _delay_us(20);
         
     }
 }
